b813ac36a4206361be970e7315afc8db401d0154
Rene F <fook_github@deinheld.de>
2021-05-11 22:45:26 +0200

    SETWORLDCOLORS: now uses real 32bit colour values

--- src/SETWORLDCOLORS.c
+++ src/SETWORLDCOLORS.c
@@ -3,14 +3,14 @@
 #include "IT2_Vars.h"
 #include "IT2_Functions.h"
 
-const worldcolors_t worldcolors[] = {   {0x00000000,0xFF000000,0xFF000000}, // cyan
-                                        {0x66000000,0x66000000,0xF7000000}, // light blue
-                                        {0xFF000000,0x00000000,0x00000000}, // red
-                                        {0x00000000,0xFF000000,0x12000000}, // green
-                                        {0xFF000000,0xFF000000,0x00000000}, // yellow
-                                        {0xBA000000,0x8B000000,0x48000000}, // brown
-                                        {0xFF000000,0x00000000,0xB0000000}, // purple
-                                        {0x77000000,0x77000000,0x77000000}  // grey
+const worldcolors_t worldcolors[] = {   {0x00000000,0xFFFFFFFF,0xFFFFFFFF}, // cyan
+                                        {0x66666666,0x66666666,0xF7F7F7F7}, // light blue
+                                        {0xFFFFFFFF,0x00000000,0x00000000}, // red
+                                        {0x00000000,0xFFFFFFFF,0x12121212}, // green
+                                        {0xFFFFFFFF,0xFFFFFFFF,0x00000000}, // yellow
+                                        {0xBABABABA,0x8B8B8B8B,0x48484848}, // brown
+                                        {0xFFFFFFFF,0x00000000,0xB0B0B0B0}, // purple
+                                        {0x77777777,0x77777777,0x77777777}  // grey
                                     };
 
 void SETWORLDCOLORS()
