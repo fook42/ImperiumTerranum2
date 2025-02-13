0a5bac47d1fc9adac78835acd175a149462ee51c
Rene F. <fook_github@deinheld.de>
2025-02-13 17:54:23 +0100

    adopted DISPLAYSTATE for size

--- src/DISPLAYSTATE.c
+++ src/DISPLAYSTATE.c
@@ -7,9 +7,9 @@ void DISPLAYSTATE()
 {
     int     punkte;
     char    s[10];
-    char    Txt_Sep[]= "-------\0";
-
-    --ActPlayer; // to shift the array
+    const char      Txt_Sep[]= "-------\0";
+    uint16  posy;
+    const uint32    imperator_state = Save.ImperatorState[ActPlayer-1];
 
     Screen2 = 0;
     SetRGB32(MyVPort_PTR[1],0,         0,         0,0x33333333);
@@ -17,41 +17,43 @@ void DISPLAYSTATE()
     SetRGB32(MyVPort_PTR[1],2,0xCCCCCCCC,         0,0x33333333);
 
     SetRast(MyRPort_PTR[1], 0); // clear Rastport
-    WRITE_RP1(320,40,1,WRITE_Center,4,_PT_HighscoreImpstatus);
+    posy = CenterY-216;
+    WRITE_RP1(CenterX,posy,1,WRITE_Center,4,_PT_HighscoreImpstatus);
 
-    WRITE_RP1(100,130,1,0,3,_PT_erreichtePunktzahl);
-    (void) dez2out(Save.ImperatorState[ActPlayer], 0, s);
-    WRITE_RP1(540,130,2,WRITE_Right,1,s);
+    posy += 50;
+    WRITE_RP1(CenterX-220,posy,1,0,3,_PT_erreichtePunktzahl);
+    (void) dez2out(imperator_state, 0, s);
+    WRITE_RP1(CenterX+220,posy,2,WRITE_Right,1,s);
 
-    WRITE_RP1(100,160,1,0,3,_PT_Levelbonus);
-    punkte = it_round(Save.ImperatorState[ActPlayer]*(Level/30.0));
+    posy += 30;
+    WRITE_RP1(CenterX-220,posy,1,0,3,_PT_Levelbonus);
+    punkte = it_round(imperator_state*((double)Level/30.0));
     (void) dez2out(punkte, 0, s);
-    WRITE_RP1(540,160,2,WRITE_Right,1,s);
+    WRITE_RP1(CenterX+220,posy,2,WRITE_Right,1,s);
 
-    WRITE_RP1(100,190,1,0,3,_PT_Spieldauer);
+    posy += 30;
+    WRITE_RP1(CenterX-220,posy,1,0,3,_PT_Spieldauer);
     if (0 < Year)
     {
         s[0] = '-';
         (void) dez2out(Year, 0, s+1);
+        punkte -= Year;
     } else {
         s[0] = '0';
         s[1] = 0;
     }
-    WRITE_RP1(540,190,2,WRITE_Right,1, s);
-
-    WRITE_RP1(540,210,2,WRITE_Right,1, Txt_Sep);
-
-    WRITE_RP1(100,240,1,0,3,_PT_Gesamtpunktzahl);
-    punkte += Save.ImperatorState[ActPlayer];
-    if (0 < Year)
-    {
-        punkte -= Year;
-    }
+    WRITE_RP1(CenterX+220,posy,2,WRITE_Right,1, s);
+    posy += 20;
+    WRITE_RP1(CenterX+220,posy,2,WRITE_Right,1, Txt_Sep);
+    posy += 30;
+    WRITE_RP1(CenterX-220,posy,1,0,3,_PT_Gesamtpunktzahl);
+    punkte += imperator_state;
     (void) dez2out(punkte, 0, s);
-    WRITE_RP1(540,240,2,WRITE_Right,1,s);
+    WRITE_RP1(CenterX+220,posy,2,WRITE_Right,1,s);
 
-    WRITE_RP1(540,270,2,WRITE_Right,1,Txt_Sep);
-    WRITE_RP1(540,275,2,WRITE_Right,1,Txt_Sep);
+    posy += 30;
+    WRITE_RP1(CenterX+220,posy,2,WRITE_Right,1,Txt_Sep);
+    WRITE_RP1(CenterX+220,posy+5,2,WRITE_Right,1,Txt_Sep);
     ScreenToFront(MyScreen[1]);
     do
     {
@@ -64,5 +66,4 @@ void DISPLAYSTATE()
         Delay(RDELAY);
     }
     while ((LMB_PRESSED || RMB_PRESSED) && (!Bool_var));
-    ++ActPlayer; // to shift the array
 }
