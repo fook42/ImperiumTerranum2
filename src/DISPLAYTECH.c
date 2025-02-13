ec06c2502d08270f64d2e3380938b0b20a8bacea
Rene F. <fook_github@deinheld.de>
2025-02-13 18:52:20 +0100

    adopted DISPLAYTECH for size

--- src/DISPLAYTECH.c
+++ src/DISPLAYTECH.c
@@ -32,7 +32,7 @@ void DISPLAYTECH(const int TechID)
     char    s[50];
     char*   _s;
     int     Depth, CivVar, Offset=0, textid=0;
-    uint16  l;
+    uint16  l, posy;
     APTR    ModC = NULL;
 
     if ((0 >= TechID) || (42 < TechID))
@@ -63,35 +63,36 @@ void DISPLAYTECH(const int TechID)
         Depth = SETCOLOR(MyScreen[1],s);
     }
     (void) my_strcpy(_s-3, "img");
-    if (!DISPLAYIMAGE(s,0,40,320,256,Depth,MyScreen[1],MyRPort_PTR[1],0)) { }
+    posy = CenterY-216;
+    if (!DISPLAYIMAGE(s,CenterX-320,posy,320,256,Depth,MyScreen[1],MyRPort_PTR[1],0)) { }
 
-    WRITE_RP1(340,50,1,0,2,PText[223]);
-    WRITE_RP1(340,70,1,0,3,TechnologyName[TechID]);
+    WRITE_RP1(CenterX+20,posy+10,1,0,2,PText[223]);
+    WRITE_RP1(CenterX+20,posy+30,1,0,3,TechnologyName[TechID]);
     if (0 < TechUse1[TechID])
     {
-        WRITE_RP1(340,110,1,0,3,PText[224]);
-        WRITE_RP1(370,130,1,0,3,TechnologyName[TechUse1[TechID]]);
+        WRITE_RP1(CenterX+20,posy+70,1,0,3,PText[224]);
+        WRITE_RP1(CenterX+50,posy+90,1,0,3,TechnologyName[TechUse1[TechID]]);
         if (TechUse1[TechID] != TechUse2[TechID])
         {
-            WRITE_RP1(370,150,1,0,3,TechnologyName[TechUse2[TechID]]);
+            WRITE_RP1(CenterX+50,posy+110,1,0,3,TechnologyName[TechUse2[TechID]]);
         }
     }
     if (27 != TechID)
     {
-        WRITE_RP1(340,190,1,0,3,PText[225]);
+        WRITE_RP1(CenterX+20,posy+150,1,0,3,PText[225]);
         l = 0;
         for(CivVar = 1; CivVar < 43; ++CivVar)
         {
             if (TechUse1[CivVar] == TechID) { l      = CivVar; }
             if (TechUse2[CivVar] == TechID) { Offset = CivVar; }
         }
-        if (0 < l) { WRITE_RP1(370,210,1,0,3,TechnologyName[l]); }
+        if (0 < l) { WRITE_RP1(CenterX+50,posy+170,1,0,3,TechnologyName[l]); }
         if ((l != Offset) && (0 < Offset) && (43 > Offset))
         {
-            WRITE_RP1(370,230,1,0,3,TechnologyName[Offset]);
+            WRITE_RP1(CenterX+50,posy+190,1,0,3,TechnologyName[Offset]);
         }
     }
-    l = 320;
+    posy += 280;
     for(CivVar = 1; CivVar < 43; ++CivVar)
     {
         if (ProjectNeedsTech[CivVar] == TechID)
@@ -105,8 +106,8 @@ void DISPLAYTECH(const int TechID)
             *_s++ = ' ';
             *_s++ = ' ';
             (void) my_strcpy(_s, ProjectName[CivVar]);
-            WRITE_RP1(20,l,1,0,3,s);
-            l += 20;
+            WRITE_RP1(CenterX-300,posy,1,0,3,s);
+            posy += 20;
         }
     }
     Screen2 = SCREEN_INVENTION;
